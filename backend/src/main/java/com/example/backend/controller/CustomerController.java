07e882ab4a425c2eb6c47692ed205073162bfb10
shamsuddha <shamsuddha@gmail.com>
2023-11-27 22:10:03 +0600

    ss

--- backend/src/main/java/com/example/backend/controller/CustomerController.java
+++ backend/src/main/java/com/example/backend/controller/CustomerController.java
@@ -1,4 +1,4 @@
 package com.example.backend.controller;
 
-public class BrandController {
+public class CustomerController {
 }
--- backend/src/main/java/com/example/backend/repository/EmployeeRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.backend.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<EmployeeRepository, Long> {
-}
